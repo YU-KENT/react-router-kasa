a2979e73bd37df0ac83c129d7fb6c5bcc871a15e
YU KENT <tianyu.tina@gmail.com>
2023-05-11 09:37:45 +0200

    correction imageslide

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -5,13 +5,13 @@ function ImageSlider({slides},{host}){
     const[currentIndex,setCurrentIndex]= useState(0)
     const goToPrevious = ()=>{
         const isFirstSlide = currentIndex === 0;
-        const newIndex = isFirstSlide ? 0 : currentIndex - 1
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1
         setCurrentIndex(newIndex)
     }
     
     const goToNext = ()=>{
         const isLastSlide = currentIndex === slides.length - 1;
-        const newIndex = isLastSlide ? slides.length - 1 : currentIndex + 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
         setCurrentIndex(newIndex)
     }
 
